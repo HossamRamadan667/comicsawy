477f21e30e4246d8b1d467c3f9ef8a4226324b84
Hossam Ramadan <hossamramadan6677@gmail.com>
2024-12-26 21:16:50 +0200

    create sound model

---
+++ lib/models/sound_model.dart
@@ -0,0 +1,15 @@
+class Sound {
+  final String id;
+  final String name;
+  final String url;
+  bool favorite;
+  final String category;
+
+  Sound({
+    required this.id,
+    required this.name,
+    required this.url,
+    required this.category,
+    required this.favorite,
+  });
+}
