8ed5e3f8f09b6e659ca370b047602014483be173
Hossam Ramadan <103776358+HossamRamadan667@users.noreply.github.com>
2025-01-30 20:50:51 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,8 @@ import 'package:flutter/material.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  
   runApp(const ProviderScope(child: App()));
 }
 
