a05fbf01ffabc491ad767278a650f628d37a5904
Hossam Ramadan <hossamramadan6677@gmail.com>
2025-02-12 21:16:21 +0200

    edit main to fit getx

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,37 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:comicsawy/middleware/add_sound_middleware.dart';
+import 'package:comicsawy/bindings/initial_binding.dart';
 import 'package:comicsawy/themes/dark_theme.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:comicsawy/firebase_options.dart';
+import 'package:comicsawy/views/add_sound.dart';
 import 'package:comicsawy/views/home/home.dart';
+import 'package:comicsawy/views/login.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
-  
-  runApp(const ProviderScope(child: App()));
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
+  Widget build(BuildContext context) => GetMaterialApp(
         debugShowCheckedModeBanner: false,
         themeMode: ThemeMode.dark,
         darkTheme: darkTheme,
-        home: const Home(),
+        initialBinding: InitialBinding(),
+        initialRoute: '/',
+        getPages: [
+          GetPage(name: '/', page: () => Home()),
+          GetPage(name: '/login', page: () => const Login()),
+          GetPage(
+              name: '/addSound',
+              page: () => const AddSound(),
+              middlewares: [AddSoundMiddleware()])
+        ],
       );
 }
