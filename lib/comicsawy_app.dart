9763375ef4c066348c831b7781a05f4c2209a8e8
Hossam Ramadan <103776358+HossamRamadan667@users.noreply.github.com>
2025-05-06 22:12:26 +0300

    initial screenutil
    - added flutter_screenutil
    - wrap material app with ScreenUtilInit

--- lib/comicsawy_app.dart
+++ lib/comicsawy_app.dart
@@ -1,6 +1,7 @@
 import 'package:comicsawy/src/core/routing/router.dart';
 import 'package:comicsawy/src/core/routing/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ComicsawyApp extends StatelessWidget {
   final AppRouter appRouter;
@@ -8,10 +9,15 @@ class ComicsawyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      onGenerateRoute: appRouter.generateRoute,
-      initialRoute: AppRoutes.home,
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      builder: (context, child) => child!,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        onGenerateRoute: appRouter.generateRoute,
+        initialRoute: AppRoutes.home,
+      ),
     );
   }
 }
