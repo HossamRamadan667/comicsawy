04a4ec246183ceab3c3368250de25bbc1d17ca30
Hossam Ramadan <103776358+HossamRamadan667@users.noreply.github.com>
2025-05-18 15:16:37 +0300

    change wrong library and add get it
    - change wrong library bloc to flutter_bloc
    - add get it library
    - fix import in soundsCubit

--- lib/src/features/home/logic/cubit/sounds_cubit.dart
+++ lib/src/features/home/logic/cubit/sounds_cubit.dart
@@ -1,8 +1,8 @@
-import 'package:bloc/bloc.dart';
 import 'package:comicsawy/src/features/home/data/models/sound_model.dart';
 import 'package:comicsawy/src/core/networking/api_result.dart';
 import 'package:comicsawy/src/features/home/data/repos/sounds_repo.dart';
 import 'package:comicsawy/src/features/home/logic/cubit/sounds_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SoundsCubit extends Cubit<SoundsState> {
   final SoundsRepo soundsRepo;
