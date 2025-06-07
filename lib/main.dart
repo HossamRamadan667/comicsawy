d6f82f2f47817250a227262e4d78f56762a5b246
Hossam Ramadan <103776358+HossamRamadan667@users.noreply.github.com>
2025-06-07 15:45:57 +0300

    initial firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:comicsawy/src/core/di/dependency_injection.dart';
 import 'package:comicsawy/src/core/routing/router.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'comicsawy_app.dart';
+// Ignored cause private
+import 'private_firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await setupGetIt();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(ComicsawyApp(appRouter: AppRouter()));
 }
