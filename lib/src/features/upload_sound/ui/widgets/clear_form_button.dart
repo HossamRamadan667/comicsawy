a0d4e7858e7c7f750fb30ff995bb02081e7af0bd
Hossam Ramadan <103776358+HossamRamadan667@users.noreply.github.com>
2025-06-30 20:15:12 +0300

    refactor clear button
    - Add functionality to clear button

--- lib/src/features/upload_sound/ui/widgets/clear_form_button.dart
+++ lib/src/features/upload_sound/ui/widgets/clear_form_button.dart
@@ -1,5 +1,7 @@
 import 'package:comicsawy/src/core/theming/text_styles.dart';
+import 'package:comicsawy/src/features/upload_sound/logic/cubit/upload_sound_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ClearFormButton extends StatelessWidget {
   const ClearFormButton({super.key});
@@ -7,10 +9,7 @@ class ClearFormButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TextButton(
-        onPressed: () {},
-        child: Text(
-          'Clear',
-          style: TextStyles.font17Gray,
-        ));
+        onPressed: BlocProvider.of<UploadSoundCubit>(context).clear,
+        child: Text('Clear', style: TextStyles.font17Gray));
   }
 }
